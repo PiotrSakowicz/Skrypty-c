d772477208c494b0fad10d7dc01c9f1501126cba
PiotrSakowicz <gawiedopiotr@gmail.com>
2020-10-28 20:09:31 +0100

    Update ProgramLiczyMałeLitery.c

--- ProgramLiczyMałeLitery.c
+++ ProgramLiczyMałeLitery.c
@@ -1,5 +1,5 @@
-/* Program sprawdza ile jest ma³ych liter
-w podanym ci¹gu znaków (tablica sta³a do 99) */
+/* Program sprawdza ile jest maÂ³ych liter
+w podanym ciÂ¹gu znakÃ³w (tablica staÂ³a do 99) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,13 +8,13 @@ w podanym ci¹gu znaków (tablica sta³a do 99) */
 
 int main()
 {
-printf ("wpisz tekst do sprawdzenia \n");
-char tab[N];
-fgets ( tab, N, stdin);
-int i, male=0;
-for(i=0; i<strlen(tab); i++)
-if (islower (tab[i]))
-male++;
-printf ("malych : %d", male);
-return 0;
+  printf ("wpisz tekst do sprawdzenia \n");
+  char tab[N];
+  fgets ( tab, N, stdin);
+  int i, male=0;
+  for(i=0; i<strlen(tab); i++)
+    if (islower (tab[i]))
+      male++;
+      printf ("malych : %d", male);
+  return 0;
 }
