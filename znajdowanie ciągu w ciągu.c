3ec3baf48924ac18d3038d3a623e1a7a6c73fdae
PiotrSakowicz <gawiedopiotr@gmail.com>
2020-10-28 19:50:42 +0100

    Update znajdowanie ciągu w ciągu.c

--- znajdowanie ciągu w ciągu.c
+++ znajdowanie ciągu w ciągu.c
@@ -3,23 +3,21 @@
 
 int main()
 {
-int i, j, k;
-char S[20]="amwambxy";
-char W[20]="bx";
-
-int ds = strlen(S);
-int dw = strlen(W);
+  int i, j, k;
+  char S[20]="amwambxy";
+  char W[20]="bx";
+  int ds = strlen(S);
+  int dw = strlen(W);
 
 for(i=0; i<ds; i++){
  for(j=0; j<dw; j++){
      printf(" i=%d, j=%d \n", i, j);
      if(S[i]==W[j]) i++;
-else break;
- }
+ else break;
+  }
  k=(i-dw)+1;
  if(j==dw){printf(" znaleziono,\n ciag o dlugosci %d , ma poczatek w elemecie %d ciagu S \n", j+1, i);
-break;
-}
-}
+ break;}
+  }
 if(j!=dw)printf("Nie Znaleziono");
 }
